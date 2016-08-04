c28c37db994f011f535dda1d099061bbba421d1e
Matthew Nicholson <matt@matt-land.com>
2016-08-03 23:35:32 -0400

    put the example from the readme in the examples/ dir

---
+++ examples/example.rs
@@ -0,0 +1,17 @@
+extern crate liner;
+
+use liner::Context;
+
+fn main() {
+    let mut con = Context::new();
+
+    loop {
+        let res = con.read_line("[prompt]$ ", &mut |_| {}).unwrap();
+
+        if res.is_empty() {
+            break;
+        }
+
+        con.history.push(res.into());
+    }
+}
