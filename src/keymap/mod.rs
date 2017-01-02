e8d920dbce21bb4d463f483a8a6c49d15aa6a1d9
Matthew Nicholson <matt@matt-land.com>
2017-01-02 17:34:04 -0500

    add KeyMap trait to abstract around key mappings
    The intention is for this class to wrap an instance of Editor and call
    into it to modify the buffer according to the user's key presses. This
    way editing operations and key presses are decoupled.

---
+++ src/keymap/mod.rs
@@ -0,0 +1,9 @@
+use std::io::{self, Write};
+use termion::event::Key;
+use Editor;
+use event::EventHandler;
+
+pub trait KeyMap<'a, W: Write, T> : From<T> {
+    fn handle_key(&mut self, key: Key, handler: &mut EventHandler<W>) -> io::Result<bool>;
+    fn editor(&mut self) -> &mut Editor<'a, W>;
+}
