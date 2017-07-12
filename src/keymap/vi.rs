2778aa6b9d753838c8a3484dc66a36b44af92322
MovingtoMars <definitelynotliam@gmail.com>
2017-07-12 15:22:35 +1200

    add test for C-h to vi keybindings

--- src/keymap/vi.rs
+++ src/keymap/vi.rs
@@ -3693,4 +3693,18 @@ mod tests {
         ]);
         assert_eq!(String::from(map), "TI_LDE");
     }
+
+    #[test]
+    /// ctrl-h should act as backspace
+    fn ctrl_h() {
+        let mut context = Context::new();
+        let out = Vec::new();
+        let ed = Editor::new(out, "prompt".to_owned(), &mut context).unwrap();
+        let mut map = Vi::new(ed);
+        map.ed.insert_str_after_cursor("not empty").unwrap();
+
+        let res = map.handle_key(Ctrl('h'), &mut |_| {});
+        assert_eq!(res.is_ok(), true);
+        assert_eq!(map.ed.current_buffer().to_string(), "not empt".to_string());
+    }
 }
